05f73962e02764f4c23bffedd9dfae909c2c24cb
Antonino <Antonino@VAIO>
2014-05-22 17:42:53 +0200

    Prova antonino commit

--- CFP/src/it/cdpaf/activity/MainActivity.java
+++ CFP/src/it/cdpaf/activity/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends Activity {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		return true; //gsdijislkdjskldm
 	}
 
 	@Override
